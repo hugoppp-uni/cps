3505f0d4fed0354b503c9db35441f2d39622835d
Hugo Protsch <hugo@protsch.dev>
2023-05-03 14:01:43 +0200

    Move StreetPosition to its own file

---
+++ src/Solution1/ConsoleApp1/StreetPosition.cs
@@ -0,0 +1,20 @@
+namespace ConsoleApp1;
+
+public struct StreetPosition
+{
+    public StreetPosition(StreetEdge streetEdge, double distanceFromSource)
+    {
+        StreetEdge = streetEdge;
+        DistanceFromSource = distanceFromSource;
+    }
+
+    public static StreetPosition WithRandomDistance(StreetEdge streetEdge)
+    {
+        return new StreetPosition(streetEdge, Random.Shared.NextDouble() * streetEdge.Length);
+    }
+
+    public override string ToString() => $"{StreetEdge.StreetName} ({DistanceFromSource:F1}m / {StreetEdge.Length:F1}m) | [src: {StreetEdge.Source.Id}, tgt: {StreetEdge.Target.Id}])";
+
+    public StreetEdge StreetEdge { get; }
+    public double DistanceFromSource { get; set; }
+}
